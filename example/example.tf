a57297a266a9042ed1b8aa22538444eeca5d60c1
brett.odgers-jewell@avanade.com <brett.odgers-jewell@avanade.com>
2023-07-30 11:10:55 +0800

    initial commit

--- example/example.tf
+++ example/example.tf
@@ -1,5 +1,5 @@
 module "azurerm_iothub_consumer_group" {
-    source = "git::https://github.com/BrettOJ/tf-az-module-iothub-consumer-group?ref=main"
+    source = "git::https://github.com/BrettOJ/tf-az-module-azurerm-iothub-consumer-group?ref=main"
     iothub_name = var.iothub_name
     eventhub_endpoint_name = var.eventhub_endpoint_name
     resource_group_name = var.resource_group_name
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "name" {
-  value = azurerm_iothub_consumer_group.example.name
+  value = azurerm_iothub_consumer_group.iot_cons_group.*.name
 }
   
